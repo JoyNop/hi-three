7d9d1decc4466d72f6ee29f8f5f0533134d6e2df
HanRui <hi@hanrui.xyz>
2023-11-08 15:08:46 +0800

    1#

---
+++ src/views/ThreeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NButton>hahah</NButton>
+
+    asdfs
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+</script>
+<style lang="scss" scoped></style>
